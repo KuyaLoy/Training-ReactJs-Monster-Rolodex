327dc3c61b9532f30a72e88a0b35332836475126
Robin Tapiru <robintapiru0894@gmail.com>
2023-07-13 18:12:09 +0400

    first-push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+    console.log("1");
+  }
+
+  componentDidMount() {
+    console.log("2");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    console.log("3");
+    return (
+      <div className="App">
+        <h1>
+          {this.state.monsters.map((monster) => {
+            return <div key={monster.id}>{monster.name}</div>;
+          })}
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
