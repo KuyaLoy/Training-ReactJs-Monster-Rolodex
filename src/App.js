535c16c832d006f15160323cc0f7c7913a5c20fa
Robin Tapiru <robintapiru0894@gmail.com>
2023-07-18 17:56:37 +0400

    44. Monsters Rolodex - Optimizations

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -26,11 +27,33 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    console.log("render");
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
-          return <h1 key={monster.id}>{monster.name}</h1>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
         })}
       </div>
     );
